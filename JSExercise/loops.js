0766e201d3fa763e6edd834579a7d4c1f871e337
tobinjohnson <tobinjohnson92@gmail.com>
2024-04-01 15:27:11 +0530

    5th

--- JSExercise/loops.js
+++ JSExercise/loops.js
@@ -1,4 +1,18 @@
+//Array of values
 let array=['apple','banana','mango']
 for(arr in array){
     console.log(array[arr])
 }
+
+//Array of objects
+let array2=[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"mango"}]
+for(let i=0; i<array2.length; i++){
+console.log(array2[i].fruit)
+}
+
+//Objects
+let object1 = { fruit1:'apple',fruit2:'banana',fruit3:'mango'}
+for( ar in object1){
+    console.log(object1[ar])
+}
+   
