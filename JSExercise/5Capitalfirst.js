dd1772a5d2953913c879498fa2c29bbbec8391e7
tobinjohnson <tobinjohnson92@gmail.com>
2024-04-01 18:51:47 +0530

    5th Completed

---
+++ JSExercise/5Capitalfirst.js
@@ -0,0 +1,16 @@
+function capitalFirstLetterSentence(str) { 
+    let ch = str[0].toUpperCase();
+    for (let i=1; i<str.length; i++) {
+           ch+=str[i]
+            if(str[i] == ' ' )
+            {
+                ch=ch+str[i+1].toUpperCase();
+                // ch=ch+str[i+1]
+                i++;
+            }
+        }
+        return ch
+
+    }
+
+console.log(capitalFirstLetterSentence("hai hello you are the first person"));
