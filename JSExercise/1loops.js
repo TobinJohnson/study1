baee52c9f0b6e27b5373fdee40a152d8c195f654
tobinjohnson <tobinjohnson92@gmail.com>
2024-04-08 15:24:01 +0530

    edited

--- JSExercise/1loops.js
+++ JSExercise/1loops.js
@@ -6,14 +6,17 @@ for(arr of array){
 
 // Array of objects
 let array2=[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"mango"}]
-for(let i=0; i<array2.length; i++){
-console.log(array2[i].fruit)
+for({fruit} of array2){
+console.log(fruit)
 }
 //Using ForEach method
 let array3=[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"mango"}]
-array3.forEach((value)=>{
-console.log(value.fruit)
-})
+array3.forEach(({fruit})=>{
+console.log(fruit)
+});
+// for({fruit} of array3){
+//     console.log(fruit)}
+
 
 
 //Objects
